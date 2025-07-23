18fc66925e700c2405167c8f286dd73b3f12456b
tanmaysawaji-1 <tanmaysawaji9@gmail.com>
2025-07-23 07:17:21 +0530

    Add files via upload

---
+++ userinformation.php
@@ -0,0 +1,12 @@
+<?php
+
+$user = $_POST['user'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+echo $user."<br>";
+echo $email."<br>";
+echo $message."<br>";
+echo "MESSAGE IS SENT";
+
+?>
